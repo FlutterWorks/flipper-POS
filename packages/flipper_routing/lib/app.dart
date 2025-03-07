52f758531c8d9a83bb87c597226fc4c2bf47af18
Muragijimana <info@yegobox.com>
2025-03-07 14:48:26 +0200

    getting ready

--- packages/flipper_routing/lib/app.dart
+++ packages/flipper_routing/lib/app.dart
@@ -803,5 +803,6 @@ class App {}
 //     AND tc.table_name = 'import_purchase_dates';
 
 // TODO: keep track https://github.com/javaherisaber/open_filex/pull/19
+// https://pub.dev/packages/pluto_layout
 
 
